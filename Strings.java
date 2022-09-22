6ce5b06263994d9705a51b1a495b08d09fa4f321
thakurlal123 <114184268+thakurlal123@users.noreply.github.com>
2022-09-23 01:17:08 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,32 @@
+// // operations on String
+
+ public class Strings {
+     public static void main(String[] args) {
+       /* String s1 ="misty";
+         String s2="thakur";
+         String s3=s1+s2;// concinate two string 
+         System.out.println(s3);
+         System.out.println(s3);
+         for(int i =0;i<s3.length();i++){
+         System.out.println(s3.charAt(i));       //to print charactor of an string
+        
+        }
+         Compare to strings.......
+        
+        String first="dauji";
+        String first1="dauji";
+    //     //s1>s2==+ve value
+         //s1==s2= 0;
+        //s1<s2=-ve value return;
+        if(first.compareTo(first1)==0){
+        System.out.println("equal");
+      }
+     else
+    //        System.out.println("not equal"); */
+    String sentence ="my name is dauji singh";
+    String name = sentence.substring(11,17);//parsing
+    System.out.println(name);
+    //strings are immutable means cant be change
+     }
+    
+ }
